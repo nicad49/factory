7f128a3a09062df9ee1091d94afabd2c2bcd4042
Doug Reimer <nicad49@gmail.com>
2016-07-12 19:51:10 -0600

    Factory Demo

---
+++ src/factory/Circle.java
@@ -0,0 +1,11 @@
+package factory;
+
+public class Circle implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Circle::draw() method");
+	}
+
+}
---
+++ src/factory/Square.java
@@ -0,0 +1,11 @@
+package factory;
+
+public class Square implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Square:draw() method.");
+	}
+
+}
